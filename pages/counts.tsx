9cba2fb6be767785737fa8c1ec317efc29d9f50b
David Baker <dmbaker90@gmail.com>
2022-07-12 01:18:56 -0400

    Fixed and wrapping up

--- pages/counts.tsx
+++ pages/counts.tsx
@@ -10,129 +10,104 @@ const Counts: NextPage = () => {
 
   const [godCountEffect, setGodCountEffect] = useState(false);
   const [womenCountEffect, setWomenCountEffect] = useState(false);
-  const [effect, setEffect] = useState(false);
 
-  //   useEffect(() => {
-  setCurrentYear(new Date().getFullYear());
-  console.log("set current year = ", currentYear);
-
-  // 2021
-  const birthsPerYear = 3659289; // USA
-  // const birthsPerYear = 3659289; // Worldwide
-
-  // Post-20 weeks
-  const stillBirthsPerYear = 24000; // USA
-  // const stillBirthsPerYear = 24000; // Worldwide
-
-  // Pre-20 weeks
+  const birthsPerYear = 3659289;
+  const stillBirthsPerYear = 24000;
   const miscarriagePercentage = 0.26;
-  const yearlyMiscarriageGod = Math.floor(
-    birthsPerYear * miscarriagePercentage
-  );
+  const yearlyAbortionsWomen = 629898;
 
-  let yearlyAbortionsGod = yearlyMiscarriageGod + stillBirthsPerYear;
-  yearlyAbortionsGod *= 10; // Testing only
-
-  // 2020
-  let yearlyAbortionsWomen = 930160; // USA
-  // const yearlyAbortionsWomen = 930160; // Worldwide
-  yearlyAbortionsWomen *= 10; // Testing only
+  const totalPregnanciesPerYear = birthsPerYear / (1 - miscarriagePercentage);
+  const yearlyMiscarriageGod = totalPregnanciesPerYear - birthsPerYear;
+  const yearlyAbortionsGod = yearlyMiscarriageGod + stillBirthsPerYear;
 
   const daysInAYear = 366;
   const minutesInAYear = daysInAYear * 24 * 60;
   const secondsInAYear = minutesInAYear * 60;
 
-  calculate();
+  setTimeout(() => {
+    calculate();
+  }, 1);
+
   useInterval(() => {
     calculate();
-  }, 5000);
+  }, 1000);
 
   function calculate() {
+    function getTotalSecondsThisYear(): number {
+      var dateTime_start = new Date(new Date().getFullYear(), 0, 1, 0, 0, 0, 0);
+      var datetime_end = new Date();
+
+      let difference =
+        Math.abs(dateTime_start.getTime() - datetime_end.getTime()) / 1000;
+      return difference;
+    }
+
+    function calculateCurrentGodCount(secondsThisYear: number) {
+      let newGodCount = Math.floor(
+        (secondsThisYear / secondsInAYear) * yearlyAbortionsGod
+      );
+      if (godCount !== newGodCount) {
+        setGodCount(newGodCount);
+        setGodCountEffect(true);
+      }
+    }
+
+    function calculateCurrentWomenCount(secondsThisYear: number) {
+      let newWomenCount = Math.floor(
+        (secondsThisYear / secondsInAYear) * yearlyAbortionsWomen
+      );
+      if (womenCount !== newWomenCount) {
+        setWomenCount(newWomenCount);
+        setWomenCountEffect(true);
+      }
+    }
+
     let secondsThisYear = getTotalSecondsThisYear();
     calculateCurrentGodCount(secondsThisYear);
     calculateCurrentWomenCount(secondsThisYear);
+    setCurrentYear(new Date().getFullYear());
   }
-
-  function getTotalSecondsThisYear(): number {
-    var dateTime_start = new Date(new Date().getFullYear(), 0, 1, 0, 0, 0, 0);
-    var datetime_end = new Date();
-
-    let difference =
-      Math.abs(dateTime_start.getTime() - datetime_end.getTime()) / 1000;
-    return difference;
-  }
-
-  function calculateCurrentGodCount(secondsThisYear: number) {
-    let newGodCount = Math.floor(
-      (secondsThisYear / secondsInAYear) * yearlyAbortionsGod
-    );
-    if (godCount !== newGodCount) {
-      console.log(
-        `godCount ${godCount} !== to ${newGodCount}, so setEffect = true now`
-      );
-      setGodCount(newGodCount);
-      setGodCountEffect(true);
-    }
-  }
-
-  function calculateCurrentWomenCount(secondsThisYear: number) {
-    let newWomenCount = Math.floor(
-      (secondsThisYear / secondsInAYear) * yearlyAbortionsWomen
-    );
-    if (womenCount !== newWomenCount) {
-      setWomenCount(newWomenCount);
-      setWomenCountEffect(true);
-    }
-  }
-  //   }, []);
 
   return (
-    <main className="flex w-full flex-1 flex-col items-center mt-10 px-20 text-center">
-      <h1
-        className={`${effect && "animate-ping"} text-6xl font-bold mb-10`}
-        onClick={() => {
-          setEffect(true);
-        }}
-        onAnimationEnd={() => setEffect(false)}
-      >
-        {currentYear} Abortions By:
-      </h1>
-
-      <div className="mt-6 flex max-w-6xl flex-wrap items-center justify-around sm:w-full">
+    <main className="flex w-full flex-1 flex-col items-center mt-10 px-20 text-center overflow-hidden">
+      <h1 className="text-7xl font-bold mb-10">{currentYear} Abortions By:</h1>
+      <div className="my-6 flex max-w-6xl flex-wrap items-center justify-around sm:w-full">
         <div className="w-full md:w-6/12 mt-10">
-          <h2 className="text-5xl font-bold">GOD</h2>
-          <p
-            className={`mt-4 text-4xl ${godCountEffect && "animate-ping"}`}
-            onAnimationEnd={() => setGodCountEffect(false)}
-          >
-            {godCount.toLocaleString()}
-          </p>
+          <h2 className="text-7xl font-bold">GOD</h2>
+          <div className="relative mt-4">
+            <p className="text-6xl absolute w-full">
+              {godCount.toLocaleString()}
+            </p>
+            <p
+              className={`text-6xl ${
+                godCountEffect && "animate-ping"
+              } relative`}
+              onAnimationEnd={() => setGodCountEffect(false)}
+            >
+              {godCount.toLocaleString()}
+            </p>
+          </div>
         </div>
         <div className="w-full md:w-6/12 mt-10">
-          <h2 className="text-5xl font-bold">Women</h2>
-          <p
-            className={`mt-4 text-4xl ${womenCountEffect && "animate-ping"}`}
-            onAnimationEnd={() => {
-              setWomenCountEffect(false);
-              //   alert(`${womenCountEffect}`);
-            }}
-          >
-            {womenCount.toLocaleString()}
-          </p>
+          <h2 className="text-7xl font-bold">Women</h2>
+
+          <div className="relative mt-4">
+            <p className="text-6xl absolute w-full">
+              {womenCount.toLocaleString()}
+            </p>
+            <p
+              className={`text-6xl ${
+                womenCountEffect && "animate-ping"
+              } relative`}
+              onAnimationEnd={() => {
+                setWomenCountEffect(false);
+              }}
+            >
+              {womenCount.toLocaleString()}
+            </p>
+          </div>
         </div>
       </div>
-
-      <button
-        className={`${
-          effect && "animate-ping"
-        } bg-blue-500 p-4 text-white rounded hover:bg-blue-700 hover:shadow-xl`}
-        onClick={() => {
-          setEffect(true);
-        }}
-        onAnimationEnd={() => setEffect(false)}
-      >
-        Wiggle wiggle...
-      </button>
     </main>
   );
 };
--- pages/faq.tsx
+++ pages/faq.tsx
@@ -3,27 +3,72 @@ import Image from "next/image";
 
 const FAQ: NextPage = () => {
   return (
-    <div>
-      <h3>FAQ</h3>
-      <div>
-        <h4>Why did you make this?</h4>
-        <p>
-          The purpose of this site is to serve as a response to the disingenous
-          attacks on pro-choice supporters by the religions right by calling
-          them murders
-        </p>
-      </div>
-      <div>
-        <h4>Where did these numbers come from?</h4>
-        <p>Test</p>
-      </div>
-      <div>
-        <h4>Do you really think miscarriages are the same as abortions?</h4>
-        <p>Test</p>
-      </div>
-      <div>
-        <h4>Don't you believe that all life is precious?</h4>
-        <p>Test</p>
+    <div className="max-w-3xl flex flex-col justify-center mx-4">
+      <h3 className="mt-96 text-4xl text-center font-bold mb-5">FAQ</h3>
+
+      <div className="">
+        <div>
+          <h4 className="faq-question">Why did you make this?</h4>
+          <p className="faq-answer">
+            The purpose of this site is to serve as a response to disingenous
+            religious pro-life supporters who call abortion murder. If
+            terminating a bundle of cells is murder, then it is important to
+            hold the biggest murderer of all accountable, God. With well over a
+            million miscarriages a year in the US, God easily kills more unborn
+            children than all abortions combined.
+          </p>
+        </div>
+        <div>
+          <h4 className="faq-question">Where did these numbers come from?</h4>
+          <p className="faq-answer">
+            In 2019, the CDC reported{" "}
+            <a
+              href="https://www.cdc.gov/mmwr/volumes/70/ss/ss7009a1.htm"
+              target="_blank"
+            >
+              629,898
+            </a>{" "}
+            abortions were performed. An estimated{" "}
+            <a
+              href="https://www.ncbi.nlm.nih.gov/books/NBK532992/"
+              target="_blank"
+            >
+              26% of pregnancies
+            </a>{" "}
+            end in miscarriage. With{" "}
+            <a
+              href="https://www.cdc.gov/nchs/data/vsrr/vsrr020.pdf"
+              target="_blank"
+            >
+              3,659,289 births
+            </a>{" "}
+            in the US, a 26% pregnancy loss would come to 1,285,696
+            misscarriages. Miscarriage only includes losses in the first 20
+            weeks gestation. Stillbirths (after 20 weeks) account for an
+            additional 24,000,{" "}
+            <a
+              href="https://www.cdc.gov/ncbddd/stillbirth/facts.html"
+              target="_blank"
+            >
+              according to the CDC
+            </a>
+            . In total this comes to 1,309,696 abortions by God.
+          </p>
+        </div>
+        <div>
+          <h4 className="faq-question">
+            Do you really think miscarriages are the same as abortions?
+          </h4>
+          <p className="faq-answer">
+            Not at all. Miscarriages, medically known as spontaneous abortions,{" "}
+          </p>
+        </div>
+        <div>
+          <h4 className="faq-question">
+            Don't you believe that all life is precious?
+          </h4>
+          <p className="faq-answer">Test</p>
+        </div>
       </div>
     </div>
   );
